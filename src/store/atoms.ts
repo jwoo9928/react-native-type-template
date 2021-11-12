8e2b508e1073321d68609ac9429da1c7bd51e5ae
Jaewoo park <jaewoopark@Jaewoos-Mac-mini.local>
2021-11-12 12:05:54 +0900

    feat : recoil-persist
    persistentAtom으로 state 저장

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,20 +1,15 @@
 //import { PersistStorage, recoilPersist } from 'recoil-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { atom, selector } from 'recoil';
-
-// const { persistAtom } = recoilPersist({
-//     key: 'recoil-persist',
-//     storage: AsyncStorage as PersistStorage
-// })
+import { persistentAtom } from 'recoil-persistence/react-native'
 
 //recoil 셋팅
-export const textState = atom<string>({
+export const textState = persistentAtom<string>({
     key:'textState', // unique ID
     default: '', //default value
-    //effects_UNSTABLE: [persistAtom],
 })
 
-export const countState = atom<Number>({
+export const countState = persistentAtom<Number>({
     key:"countState",
     default : 0
 })
