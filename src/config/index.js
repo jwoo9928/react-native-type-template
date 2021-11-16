beff3f0d08f2c0910fc008da56eb80119681303f
jwoo9928 <astro9928@o.cnu.ac.kr>
2021-11-16 10:18:55 +0900

    feat : device config

--- src/config/index.js
+++ src/config/index.js
@@ -1,37 +1,36 @@
 import {
     Dimensions,
     Platform,
-  } from 'react-native';
-  
-  import {HOST, PORT, BASE_PATH, SERVERTYPE} from '@env';
-  
-  const config = {
+} from 'react-native';
+
+import { HOST, PORT, BASE_PATH, SERVERTYPE } from '@env';
+
+const config = {
     api: {
-      host: HOST,
-      port: PORT,
-      path: BASE_PATH,
+        host: HOST,
+        port: PORT,
+        path: BASE_PATH,
     },
-  };
-  
-  const APP_VERSION = {
-      android : '0.0.1',
-      ios : '0.0.1'
-  }
-  
-  const PLATFORM = Platform.select({
+};
+
+const APP_VERSION = {
+    android: '0.0.1',
+    ios: '0.0.1'
+}
+
+const PLATFORM = Platform.select({
     android: 'android',
     ios: 'ios'
-  })
-  
-  const WINDOW_HEIGHT = Dimensions.get('window').height;
-  const WINDOW_WIDTH  = Dimensions.get('window').width;
-  
-  export default {
+})
+
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const WINDOW_WIDTH = Dimensions.get('window').width;
+
+export default {
     API_PATH,
     PLATFORM,
     WINDOW_HEIGHT,
     WINDOW_WIDTH,
     SERVERTYPE,
     APP_VERSION
-  };
-  
+};
