db7e10fda0d6a01037aacec9942a9661069a31da
jwoo9928 <astro9928@o.cnu.ac.kr>
2021-11-22 12:19:55 +0900

    fix : 불필요한 변수 제거

--- src/store/DebugObserver.tsx
+++ src/store/DebugObserver.tsx
@@ -1,46 +1,43 @@
-import React, { useRef, useState } from 'react';
 import { RecoilState, Snapshot, useRecoilTransactionObserver_UNSTABLE } from 'recoil';
-import { textState, walletStates } from './atoms';
 
-interface observerProps  {
-    states :  (RecoilState<string> | RecoilState<object[]> | RecoilState<number>)[]
+interface observerProps {
+    states: (RecoilState<string> | RecoilState<object[]> | RecoilState<number>)[]
 }
 
 interface keyValuePair {
-    key : string,
-    value :  RecoilState<any>
+    key: string,
+    value: RecoilState<any>
 }
 
 
-const DebugObserver = ({states} : any) => {
+const DebugObserver = ({ states }: any) => {
     useRecoilTransactionObserver_UNSTABLE(({ snapshot, previousSnapshot }) => {
         console.group("action Atom")
-        console.log("%c previous state : ",'color: #E6E6FA',getState(previousSnapshot));
-        console.log("%c action","color : #00FFFF",changedState(snapshot));
-        console.log("%c next state : ",'color: #bada55',getState(snapshot));
+        console.log("%c previous state : ", 'color: #E6E6FA', getState(previousSnapshot));
+        console.log("%c action", "color : #00FFFF", changedState(snapshot));
+        console.log("%c next state : ", 'color: #bada55', getState(snapshot));
     })
 
-    const getState = (snapShot : Snapshot) => {
+    const getState = (snapShot: Snapshot) => {
         const preValues: any[] = [];
-        states.map((item : keyValuePair) => {
-            const result = snapShot.getPromise(item.value).then((result : any) => preValues.push({[item.key] : result}))
+        states.map((item: keyValuePair) => {
+            snapShot.getPromise(item.value).then((result: any) => preValues.push({ [item.key]: result }))
         })
         return preValues;
     }
 
-    const changedState = (snapShot : Snapshot) => {
+    const changedState = (snapShot: Snapshot) => {
         let result = {};
-        states.map((item : keyValuePair) => {
-            //console.log(`key : ${item.key} :`,snapShot.getInfo_UNSTABLE(item.value).loadable)
+        states.map((item: keyValuePair) => {
             let snapshotInfo = snapShot.getInfo_UNSTABLE(item.value);
             if (snapshotInfo.isModified) {
-                result = {[item.key]: snapshotInfo.loadable?.contents}
+                result = { [item.key]: snapshotInfo.loadable?.contents }
             }
         })
-        return result
+        return result;
     }
-   
-    return(<></>)
+
+    return (<></>)
 }
 
 export default DebugObserver;
